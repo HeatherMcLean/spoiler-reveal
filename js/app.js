cb15ed9d7d4dfb285bacce733e0c7c37030e9f87
HeatherMcLean <heatherabrey@gmail.com>
2018-11-06 11:36:18 +0000

    add app.js to html doc and add jquery to show spoiler and hide button on click

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
+//Hide spoiler on page load
+$('.spoiler span').hide();
 
+//When button is pressed
+$('.spoiler button').click(function() {                 
+  //Reveal spoiler
+  $('.spoiler span').show();
+  //Hide button
+  $('.spoiler button').hide();
+});
 
 
 
